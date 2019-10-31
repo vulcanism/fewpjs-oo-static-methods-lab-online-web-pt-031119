851737fb125f7e21360e7af39cfe3ede14417930
Savannah C <sgcorrell@gmail.com>
2019-10-31 15:08:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ class Formatter {
   }
 
   static titleize(string) {
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let titleString = []
+    let stringArray = string.split(" ")
 
+    for (let i = 0; i < stringArray.length; i++) {
+      if (i === 0) {
+        titleString.push(this.capitalize(stringArray[i]))
+      } else if (exceptions.includes(stringArray[i])){
+        titleString.push(stringArray[i])        
+      } else {
+        titleString.push(this.capitalize(stringArray[i]))
+      }
+    }
   }
 }
