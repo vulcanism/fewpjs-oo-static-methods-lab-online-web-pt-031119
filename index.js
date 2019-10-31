c2b8bf639a3296070639765b38b39d6a7d457eaf
Savannah C <sgcorrell@gmail.com>
2019-10-31 15:02:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Formatter {
   }
 
   static sanitize(string) {
-    return string.replace(/[A-Za-z0-9-]+/g, '')
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
   }
 
   static titleize(string) {
